923fe6dffc705be06a049b889e2f7b9e36a23803
Sahil Jadhav <sahilmj1805@gmail.com>
2026-02-12 14:23:23 +0000

    Added admin rote

---
+++ routes/admin.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+adminRouterRouter.post("/signup", function(req, res){
+
+})
+
+adminRouterRouter.post("/signin", function(req, res){
+    
+})
+
+adminRouterRouter.post("/course", function(req, res){
+
+})
+
+adminRouterRouter.put("/course", function(req, res){
+
+})
+
+adminRouterRouter.get("/course/bulk", function(req, res){
+
+})
+
+module.exports = {
+    adminRouter: adminRouter
+};
