792a1b4cc80a39467b0612260834802472005c00
Sahil Jadhav <sahilmj1805@gmail.com>
2026-02-12 14:09:31 +0000

    Routing introduced to codebase

---
+++ routes/course.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const courseRouter = Router();
+
+courseRouter.post("/purchase", function(req, res){
+    
+})
+
+courseRouter.post("/view", function(req, res){
+    
+})
+
+module.exports = {
+    courseRouter: courseRouter
+}
