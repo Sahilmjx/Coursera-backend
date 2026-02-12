30adbd0659788f95d1268b2822aaf48289ed4bb3
Sahil Jadhav <sahilmj1805@gmail.com>
2026-02-12 19:46:38 +0000

    Added database connection and defined db schema

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
@@ -9,5 +10,10 @@ app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 app.use("/course", courseRouter);
 
+async function main(){
+    await mongoose.connect("mongodb+srv://admin:EtGfWYdiovU7bG4w@cluster0.ia3oiue.mongodb.net/coursera-app");
+    app.listen(3000);
+    console.log("Listning on port 3000");
+}
 
-app.listen(3000);
+main();
--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,24 @@
 const { Router } = require("express");
 const adminRouter = Router();
+const { adminModel } = require("../db");
 
-adminRouterRouter.post("/signup", function(req, res){
+adminRouter.post("/signup", function(req, res){
 
 })
 
-adminRouterRouter.post("/signin", function(req, res){
+adminRouter.post("/signin", function(req, res){
     
 })
 
-adminRouterRouter.post("/course", function(req, res){
+adminRouter.post("/course", function(req, res){
 
 })
 
-adminRouterRouter.put("/course", function(req, res){
+adminRouter.put("/course", function(req, res){
 
 })
 
-adminRouterRouter.get("/course/bulk", function(req, res){
+adminRouter.get("/course/bulk", function(req, res){
 
 })
 
