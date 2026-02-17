345f56de01da51890b7b53506b260e8199654e3f
Sahil Jadhav <sahilmj1805@gmail.com>
2026-02-17 14:59:25 +0000

    Completed all admin endpoints

--- routes/admin.js
+++ routes/admin.js
@@ -65,12 +65,40 @@ adminRouter.post("/course", adminMiddleware, async function(req, res){
 
 })
 
-adminRouter.put("/course", function(req, res){
+adminRouter.put("/course", adminMiddleware, async function(req, res){
 
+    const adminId = req.userId;
+
+    const { title, description, price, imageurl, courseId } = req.body;
+
+    const course = await courseModel.updateOne({
+        _id: courseId,
+        creatorId: adminId
+    }, {
+        title,
+        description,
+        price,
+        imageurl
+    });
+
+    res.json({
+        message: "Course updated",
+        courseId: course._id
+    })
 })
 
-adminRouter.get("/course/bulk", function(req, res){
+adminRouter.get("/course/bulk", adminMiddleware, async function(req, res){
 
+    const adminId = req.userId;
+
+    const courses = await courseModel.find({
+        creatorId: adminId
+    });
+
+    res.json({
+        message: "Your courses are",
+        courses
+    });
 })
 
 module.exports = {
